a32cbfe0d6ab71e1d37624aca2c43628f68fc49d
ls3893 <ls3893@desales.edu>
2025-04-15 14:06:03 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -8,43 +8,44 @@ import java.io.IOException;
 
 public class Game
 {
-		public HashMap<String, HashMap<String, Integer>> Minecraft()
-		{
+	public HashMap<String, HashMap<String, Integer>> Minecraft()
+	{
 		String csvFile = "src/allcrafting-mc.csv";
 		
-			ArrayList<String> list = new ArrayList<>();
-			list = readWithBR(csvFile);
+		ArrayList<String> list = new ArrayList<>();
+		list = readWithBR(csvFile);
 			
-			HashMap<String, HashMap<String, Integer>> basic = new HashMap<>();
-			HashMap<String, Integer> tempHM = new HashMap<>();
-			for(int i = 0; i < list.size(); i++)
+		HashMap<String, HashMap<String, Integer>> basic = new HashMap<>();
+		HashMap<String, Integer> tempHM = new HashMap<>();
+		for(int i = 0; i < list.size(); i++)
+		{
+			if(list.get(i).contains("Basic"))
 			{
-				if(list.get(i).contains("Basic"))
-				{
-					String tempList1 = list.get(i);
-					String[] tempList2 = tempList1.split(",");
-					//System.out.println(Arrays.toString(tempList2));
+				String tempList1 = list.get(i);
+				String[] tempList2 = tempList1.split(",");
+				//System.out.println(Arrays.toString(tempList2));
 					
 					
-					tempHM.put("Quantity", Integer.parseInt(tempList2[1]));
+				tempHM.put("Quantity", Integer.parseInt(tempList2[1]));
+				basic.put(tempList2[2], tempHM);
+					
+				int k = 3, v = 4;
+				for(int j = 0; j < tempList2.length-4; j+=2)
+				{	
+					tempHM.put(tempList2[k], Integer.parseInt(tempList2[v]));
 					basic.put(tempList2[2], tempHM);
-					
-					int k = 3, v = 4;
-					for(int j = 0; j < tempList2.length-4; j+=2)
-					{	
-						tempHM.put(tempList2[k], Integer.parseInt(tempList2[v]));
-						basic.put(tempList2[2], tempHM);
-						k += 2;
-						v += 2;
-					}
-					for(String z : basic.keySet())
-					{
+					k += 2;
+					v += 2;
+				}
+				for(String z : basic.keySet())
+				{
 						System.out.println("key: " + z + "\nvalue" + basic.get(z));
-					}
 				}
 			}
-			return basic;
 		}
+		
+		return basic;
+	}
 	
 	public void StardewValley()
 	{
